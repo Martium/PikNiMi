4465ee7f2e36694261c77fbfe0ab860408d16894
Martynas Gedutis <martynasgedutis@gmail.com>
2022-01-21 15:15:04 +0200

    FAKE REPOSITORY begin

---
+++ PikNiMi/PikNiMi/Repository/SqlLite/FakeRepository.cs
@@ -0,0 +1,22 @@
+﻿namespace PikNiMi.Repository.SqlLite
+{
+    public class FakeRepository
+    {
+        private const string FullProductInfoTable = "FullProductInfoTable";
+
+
+        public void FillTestingInfoForProduct()
+        {
+
+            string testingInfo = 
+                $@"
+                    BEGIN TRANSACTION;
+                    INSERT INTO '{FullProductInfoTable}'
+                    VALUES (
+                    );
+                    COMMIT;
+                  
+                ";
+        }
+    }
+}
