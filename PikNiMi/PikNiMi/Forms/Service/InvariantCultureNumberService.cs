5e1da90c16856f4bc1c55230776a954b4d9de728
Martynas Gedutis <69677885+Martium@users.noreply.github.com>
2022-01-30 00:19:05 +0200

    Revert "Revert "Fake  repository""

---
+++ PikNiMi/PikNiMi/Forms/Service/InvariantCultureNumberService.cs
@@ -0,0 +1,55 @@
+﻿using System.Globalization;
+using PikNiMi.Interface.NumbersServiceInterface;
+
+namespace PikNiMi.Forms.Service
+{
+    public class InvariantCultureNumberService : INumberService
+    {
+        private readonly CultureInfo _cultureInfo = CultureInfo.InvariantCulture;
+        private readonly NumberStyles _numberStyles = NumberStyles.Any;
+
+        private string ChangeCommaToDot(string text)
+        {
+            return text.Replace(",", ".");
+        }
+
+        public int ParseStringToNumber(string text)
+        {
+            return int.Parse(text);
+        }
+
+        public int? TryParseStringToNumberOrNull(string text)
+        {
+            int? numberOrNull;
+
+            if (int.TryParse(text, out int number))
+            {
+                numberOrNull = number;
+            }
+            else
+            {
+                numberOrNull = null;
+            }
+
+            return numberOrNull;
+        }
+
+        public double? TryParseStringToDoubleNumberOrNull(string text)
+        {
+            string changeCommaToDot = text != null ? ChangeCommaToDot(text) : null;
+
+            double? numberOrNull;
+
+            if (double.TryParse(changeCommaToDot, _numberStyles, _cultureInfo ,out double number))
+            {
+                numberOrNull = number;
+            }
+            else
+            {
+                numberOrNull = null;
+            }
+
+            return numberOrNull;
+        }
+    }
+}
